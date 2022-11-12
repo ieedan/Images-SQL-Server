febf130e927aa830102e1cd03645bd2fdf467300
Aidan Bleser <aidanbleser35@gmail.com>
2022-11-12 08:52:23 -0600

    Code Commit
    All the code to store and retrieve images is here

---
+++ StoreAndRetrieveImage/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace StoreAndRetrieveImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.image_store = new System.Windows.Forms.PictureBox();
+            this.image_retrieve = new System.Windows.Forms.PictureBox();
+            this.btn_store = new System.Windows.Forms.Button();
+            this.btn_retrieve = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.num_imgid = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.image_store)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image_retrieve)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_imgid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image_store
+            // 
+            this.image_store.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.image_store.Location = new System.Drawing.Point(23, 99);
+            this.image_store.Name = "image_store";
+            this.image_store.Size = new System.Drawing.Size(327, 250);
+            this.image_store.TabIndex = 0;
+            this.image_store.TabStop = false;
+            this.image_store.Click += new System.EventHandler(this.image_store_Click);
+            // 
+            // image_retrieve
+            // 
+            this.image_retrieve.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.image_retrieve.Location = new System.Drawing.Point(553, 99);
+            this.image_retrieve.Name = "image_retrieve";
+            this.image_retrieve.Size = new System.Drawing.Size(327, 250);
+            this.image_retrieve.TabIndex = 1;
+            this.image_retrieve.TabStop = false;
+            // 
+            // btn_store
+            // 
+            this.btn_store.Location = new System.Drawing.Point(113, 397);
+            this.btn_store.Name = "btn_store";
+            this.btn_store.Size = new System.Drawing.Size(115, 73);
+            this.btn_store.TabIndex = 2;
+            this.btn_store.Text = "Store";
+            this.btn_store.UseVisualStyleBackColor = true;
+            this.btn_store.Click += new System.EventHandler(this.btn_store_Click);
+            // 
+            // btn_retrieve
+            // 
+            this.btn_retrieve.Location = new System.Drawing.Point(658, 397);
+            this.btn_retrieve.Name = "btn_retrieve";
+            this.btn_retrieve.Size = new System.Drawing.Size(115, 73);
+            this.btn_retrieve.TabIndex = 3;
+            this.btn_retrieve.Text = "Retrieve";
+            this.btn_retrieve.UseVisualStyleBackColor = true;
+            this.btn_retrieve.Click += new System.EventHandler(this.btn_retrieve_Click);
+            // 
+            // num_imgid
+            // 
+            this.num_imgid.Location = new System.Drawing.Point(658, 356);
+            this.num_imgid.Name = "num_imgid";
+            this.num_imgid.Size = new System.Drawing.Size(120, 26);
+            this.num_imgid.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 554);
+            this.Controls.Add(this.num_imgid);
+            this.Controls.Add(this.btn_retrieve);
+            this.Controls.Add(this.btn_store);
+            this.Controls.Add(this.image_retrieve);
+            this.Controls.Add(this.image_store);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image_store)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image_retrieve)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_imgid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image_store;
+        private System.Windows.Forms.PictureBox image_retrieve;
+        private System.Windows.Forms.Button btn_store;
+        private System.Windows.Forms.Button btn_retrieve;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NumericUpDown num_imgid;
+    }
+}
+
---
+++ StoreAndRetrieveImage/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoreAndRetrieveImage
+{
+    public partial class Form1 : Form
+    {
+        string connectionstring = "Connection string here"; // Replace this with your connection string
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void image_store_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog(); // Open dialog to pick image
+            dialog.ShowDialog();
+
+            if (dialog.FileName != null)
+            {
+                var filepath = dialog.FileName;
+                image_store.ImageLocation = filepath; // If image was picked update screen image
+            }
+        }
+
+        private void btn_store_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlconnection = new SqlConnection(
+                connectionstring))
+            {
+                sqlconnection.Open();
+
+                // Converts image file into byte[]
+                byte[] imgData = File.ReadAllBytes(image_store.ImageLocation);
+
+                string insertXmlQuery = @"Insert Into [Images] (Description,Image) Values('This is an image',@Photo)";
+
+                // Insert Image Value into Sql Table by SqlParameter
+                SqlCommand insertCommand = new SqlCommand(insertXmlQuery, sqlconnection);
+                SqlParameter sqlParam = insertCommand.Parameters.AddWithValue("@Photo", imgData);
+                sqlParam.DbType = DbType.Binary;
+                insertCommand.ExecuteNonQuery();
+            }
+        }
+
+        private void btn_retrieve_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlconnection = new SqlConnection(connectionstring))
+            {
+                sqlconnection.Open();
+
+                string selectQuery = string.Format(@"Select [Image] From [Images] Where Id={0}",
+                                     num_imgid.Value);
+
+                //Read Image Value from Sql Table that was stored as Image Datatype
+                SqlCommand selectCommand = new SqlCommand(selectQuery, sqlconnection);
+                SqlDataReader reader = selectCommand.ExecuteReader();
+                if (reader.Read())
+                {
+                    byte[] imgData = (byte[])reader[0];
+                    using (MemoryStream ms = new MemoryStream(imgData))
+                    {
+                        System.Drawing.Image image = Image.FromStream(ms);
+                        image_retrieve.Image = image;
+                    }
+                }
+            }
+        }
+    }
+}
